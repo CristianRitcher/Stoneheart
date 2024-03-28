1a5d236d8692be40a20202dd7cc7fd88b2282877
Cristian Ritcher <cristian.ritcher@outlook.com>
2024-03-28 11:41:57 -0600

    Initial Commit

---
+++ Stoneheart/StoneheartApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoneheartApp.swift
+//  Stoneheart
+//
+//  Created by Cristian Ritcher on 28/03/24.
+//
+
+import SwiftUI
+
+@main
+struct StoneheartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

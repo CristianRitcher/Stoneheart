53bea65510e5b9b396c09d08b03569b4ebb917f6
Cristian Ritcher <cristian.ritcher@outlook.com>
2024-03-28 12:25:38 -0600

    LogIn enviromental variable

--- Stoneheart/👁️ Views/tabView.swift
+++ Stoneheart/👁️ Views/tabView.swift
@@ -1,5 +1,18 @@
-import Foundation
+//
+//  tabView.swift
+//  Stoneheart
+//
+//  Created by Cristian Ritcher on 28/03/24.
+//
 
+import SwiftUI
 
+struct tabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
 
-
+#Preview {
+    tabView()
+}

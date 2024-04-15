f6ab9621d073d1e38cf9417877a58c17db5ddb4c
Cristian Ritcher <cristian.ritcher@outlook.com>
2024-04-15 02:41:11 -0600

    ddd

--- Stoneheart/🎮 Controllers/generarVacantes.swift
+++ Stoneheart/🎮 Controllers/generarVacantes.swift
@@ -6,13 +6,30 @@
 //
 
 import SwiftUI
+import Firebase
 
-struct generarVacantes: View {
+struct User: Identifiable {
+    var id: String
+    var name: String
+    var email: String
+    // Otros campos que puedas tener en tus documentos de Firebase
+}
+
+struct UserCardView: View {
+    var user: User
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text(user.name)
+                .font(.title)
+            Text(user.email)
+                .font(.subheadline)
+                .foregroundColor(.gray)
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 5)
     }
 }
 
-#Preview {
-    generarVacantes()
-}

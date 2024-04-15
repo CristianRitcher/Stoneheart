db419ce1a7b533d379a77804709e444b1421c836
Cristian Ritcher <cristian.ritcher@outlook.com>
2024-04-15 02:40:40 -0600

    ddd

---
+++ Stoneheart/🎮 Controllers/generarVacantes.swift
@@ -0,0 +1,18 @@
+//
+//  generarVacantes.swift
+//  Stoneheart
+//
+//  Created by Cristian Ritcher on 15/04/24.
+//
+
+import SwiftUI
+
+struct generarVacantes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    generarVacantes()
+}
